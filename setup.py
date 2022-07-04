2264c02c1a84c03a0e79eb76bab39623a84e1f13
cocoquiet <cocoquiet@knu.ac.kr>
2022-07-04 16:03:42 +0900

    CHANGE: Loop site name storing

--- setup.py
+++ setup.py
@@ -1,20 +1,33 @@
 import requests
+import json
 from bs4 import BeautifulSoup
+import time
 
 
-# for page in range(1, 36):
-    # url = f'http://api.namechart.kr/chart/overall?gender=t&page={page}&size=100'
+session = requests.Session()
 
-url = 'https://api.namechart.kr/chart/overall?gender=t&page=1&size=100'
+for page in range(1, 36):
+    url = f'http://api.namechart.kr/chart/overall?gender=t&page={page}&size=100'
 
+    try:
+        response = session.get(url, verify=False)
+    except:
+        print('에러: 2초 딜레이')
+        time.sleep(2)
+        response = session.get(url, verify=False)
 
-response = requests.get(url, verify=False)
+    if response.status_code == 200:
+        print(f'page {page} start')
+        
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        with open('nameList', 'a', encoding='euc-kr') as f:
+            for item in json.loads(soup.text)['items']:
+                name = item['name']
+                f.write(f'{name}\n')
 
-if response.status_code == 200:
-    html = response.text
-    soup = BeautifulSoup(html, 'html.parser')
-    f = open('nameDB.json')
-    f.write(soup)
+        print(f'page {page} end')
 
-else : 
-    print(response.status_code)
+    else : 
+        print(response.status_code)
+        break
